39a07d61526af728cd65a2ab28e24e644b6419cb
glmack <glmack@gmail.com>
2022-01-21 18:03:17 -0800

    Begin script to download Brazil geo datasets

---
+++ get_br_geo_datasets.py
@@ -0,0 +1,43 @@
+import requests
+
+# DTB_2020_v2.zip, version 2021-02-25 10:07, size 1.8M	 
+# https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2020/
+def get_dtb_2020_v2():
+    resp = requests.get()
+    pass
+
+# Regic
+# https://www.ibge.gov.br/geociencias/cartas-e-mapas/redes-geograficas/15798-regioes-de-influencia-das-cidades.html?=&t=downloads
+# https://www.ibge.gov.br/#
+def get_br_regic2018():
+    resp = requests.get()
+    pass
+
+# Geonames Brazil zip file
+# http://download.geonames.org/export/dump/BR.zip
+def get_br_geonames():
+    resp = requests.get()
+    pass
+
+# Geonet names Brazil file, version 2022-01-07
+# https://geonames.nga.mil/gns/html/cntyfile/br.zip
+def get_br_geonet():
+    resp = requests.get()
+    pass
+
+# TODO (Lee): confirm versions/links
+# Geographical Names Index (Índice de Nomes Geográficos), v1 
+# Brazil to the Millionth Continuous Cartographic Base Map (Base Cartográfica Contínua do Brasil ao Milionésimo) BCIM
+# https://geoftp.ibge.gov.br/cartas_e_mapas/bases_cartograficas_continuas/bcim/versao2016/lista_de_nomes_geograficos/csv/
+# https://www.ibge.gov.br/geociencias/downloads-geociencias.html
+def get_br_nomes_geograficos_bcim():
+    resp = requests.get()
+    pass
+
+
+# Arranjos Populacionais e Concentrações Urbanas do Brasil, 2nd edition, 2016
+
+
+
+# RIDE
+https://dados.gov.br/dataset/ea854217-9727-4c07-a483-1cee82a52322/resource/36643313-179e-4543-ac94-ec560181c2e2
